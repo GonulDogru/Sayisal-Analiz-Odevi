f800dd4558fd50d723eb96a2c77c3910ef088517
GonulDogru <149140035+GonulDogru@users.noreply.github.com>
2023-11-19 21:24:51 +0300

    Add files via upload

---
+++ Sayısal Analiz Ödevi/ikiyeBölmeMetoduOdevi.py
@@ -0,0 +1,28 @@
+def f(x):
+    return x*3 - 2*x*2 - 5
+
+def ikiye_bolme(a, b, tol, max_iter):
+    for i in range(max_iter):
+        c = (a + b) / 2
+        if f(c) == 0 or (b - a) / 2 < tol:
+            return c
+        elif f(c) * f(a) < 0:
+            b = c
+        else:
+            a = c
+    return c
+
+# İkiye bölme yöntemiyle kök bulma
+a = 2
+b = 4
+tolerans = 0.0001
+max_iterasyon = 4
+
+kok = ikiye_bolme(a, b, tolerans, max_iterasyon)
+print(f"[2,4] aralığında bulunan kök: {kok}")
+
+
+
+
+
+
---
+++ Sayısal Analiz Ödevi/newtonRaphsonOdevi.py
@@ -0,0 +1,34 @@
+
+import math
+
+def f(x):
+    return (4 * math.exp(-0.5 * x)) - x
+
+def derivative_f(x, h=1e-5):
+    return (f(x + h) - f(x)) / h
+
+def newton_raphson_method(x0, tol, max_iter):
+    iter_count = 0
+    x = x0
+    
+    while True:
+        if derivative_f(x) == 0:
+            print("Türev sıfır olduğu için bölme hatası oluştu.")
+            return None
+        
+        x_next = x - f(x) / derivative_f(x)
+        
+        if abs(x_next - x) < tol or iter_count >= max_iter:
+            return x_next
+        
+        x = x_next
+        iter_count += 1
+
+# Başlangıç değeri ve diğer parametreleri belirleyelim
+initial_guess = 2
+tolerance = 1e-8  # Hata toleransı
+max_iterations = 4
+
+root = newton_raphson_method(initial_guess, tolerance, max_iterations)
+if root is not None:
+    print(f"Newton-Raphson ile bulunan kök: {root}")
